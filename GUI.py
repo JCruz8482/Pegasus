de3a644206a11e8e1d0e8b70de8ec341ee98eaa7
Keegan Jordan <keeganbbjordan@Keegans-MacBook-Pro.local>
2017-07-20 19:54:13 -0700

    Added canvas and track to GUI

--- GUI.py
+++ GUI.py
@@ -2,4 +2,11 @@
 
 import tkinter
 top = tkinter.Tk()
+
+Canvas = tkinter.Canvas(top, bg="grey", height=400, width=400)
+
+coord = 10, 100, 300, 200
+
+track = Canvas.create_oval(coord, width=)
+Canvas.pack()
 top.mainloop();
